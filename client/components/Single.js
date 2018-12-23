1df1e18743c7efcf3534adaac2cc57c6329ee754
Your Name <ilin96inbox@gmail.com>
2018-12-23 22:31:53 +0800

    display single post

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Photo from "./Photo";
+
+import Comments from "./Comments";
 
 const Single = React.createClass({
   render() {
-    return <div className="single-photo">Singe</div>;
+    const i = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+      </div>
+    );
   }
 });
 
