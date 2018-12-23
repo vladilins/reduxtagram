11157f5ae61cb9158aec193e56152e01931d7b18
Your Name <ilin96inbox@gmail.com>
2018-12-23 18:43:15 +0800

    router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,5 +4,18 @@ import { render } from "react-dom";
 import css from "./styles/style.styl";
 
 import Main from "./components/Main";
+import Single from "./components/Single";
+import PhotoGrid from "./components/PhotoGrid";
 
-render(<Main />, document.getElementById("root"));
+import { Router, Route, IndexRoute, browserHistorey } from "react-router";
+
+const router = (
+  <Router history={browserHistorey}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single} />
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById("root"));
