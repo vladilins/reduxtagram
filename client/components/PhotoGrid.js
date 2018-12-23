c733711c5a86fb1f30bd882bdc42da555510637e
Your Name <ilin96inbox@gmail.com>
2018-12-23 18:34:46 +0800

    sing and photo grid

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PhotoGrid = React.createClass({
+  render() {
+    return <div className="photo-grid">s</div>;
+  }
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Single = React.createClass({
+  render() {
+    return <div className="single-photo">s</div>;
+  }
+});
+
+export default Single;
